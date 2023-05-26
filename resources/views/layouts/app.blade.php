6923046e165837578881336418713c71830b7802
lancenacabuan <lancenacabuan@outlook.com>
2023-05-26 11:24:25 +0800

    Localize Includes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,17 +22,18 @@
         <link href="/mg-logo.ico" rel="shortcut icon" type="image/x-icon"/>
     @endif
     <link href="https://fonts.gstatic.com/" rel="preconnect" crossorigin>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/{{$jquery_version}}/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap{{$bootstrap_version}}/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/{{$datatables_version}}/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/sweetalert2{{$sweetalert_version}}/dist/sweetalert2.min.css" rel='stylesheet'>
-    <link href="https://unpkg.com/multiple-select@1.5.2/dist/multiple-select.min.css" rel="stylesheet">
-    <link href="https://code.jquery.com/ui/{{$jquery_ui_version}}/themes/base/jquery-ui.css" rel="stylesheet">
+    <script src="/js/inc/jquery.min.js"></script>
+    <link rel="stylesheet" href="/css/inc/bootstrap4.min.css">
+    <link rel="stylesheet" href="/css/inc/bootstrap5.min.css">
+    <link rel="stylesheet" href="/css/inc/bootstrap-icons.css">
+    <link rel="stylesheet" href="/css/inc/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="/css/inc/buttons.dataTables.min.css">
+    <link href="/fontawesome-free-6.2.0-web/css/all.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="/css/inc/sweetalert2.min.css">
+    <link rel="stylesheet" href="/css/inc/multiple-select.min.css">
+    <link rel="stylesheet" href="/css/inc/jquery-ui.css">
+    <link rel="stylesheet" href="/css/inc/daterangepicker.css">
     <link href="/css/inc/chosen.css" rel="stylesheet" type="text/css"/>
 
     @if(env('APP_SYS') == 'DD')
@@ -98,22 +99,19 @@
     <main class="container-fluid content">
         @yield('content')
     </main>
-    <script src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js" type="text/javascript"></script>
-    <link href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" rel="stylesheet" type="text/css"/>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap{{$bootstrap_version}}/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.datatables.net/{{$datatables_version}}/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/fixedcolumns/4.2.2/js/dataTables.fixedColumns.min.js"></script>
-
-    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.8.1/html2pdf.bundle.min.js" integrity="sha512-vDKWohFHe2vkVWXHp3tKvIxxXg0pJxeid5eo+UjdjME3DBFBn2F8yWOE0XmiFcFbXxrEOR1JriWEno5Ckpn15A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2{{$sweetalert_version}}/dist/sweetalert2.all.min.js"></script>
-    <script src="https://code.jquery.com/ui/{{$jquery_ui_version}}/jquery-ui.min.js"></script>
+    <script src="/js/inc/daterangepicker.js"></script>
+    <script src="/js/inc/bootstrap4.bundle.min.js"></script>
+    <script src="/js/inc/bootstrap5.bundle.min.js"></script>
+    <script src="/js/inc/jquery.dataTables.min.js"></script>
+    <script src="/js/inc/dataTables.fixedColumns.min.js"></script>
+    <script src="/js/inc/dataTables.buttons.min.js"></script>
+    <script src="/js/inc/pdfmake.min.js"></script>
+    <script src="/js/inc/vfs_fonts.js"></script>
+    <script src="/js/inc/buttons.html5.min.js"></script>
+    <script src="/js/inc/jszip.min.js"></script>
+    <script src="/js/inc/html2pdf.bundle.min.js"></script>
+    <script src="/js/inc/sweetalert2.all.min.js"></script>
+    <script src="/js/inc/jquery-ui.min.js"></script>
     <script src="/js/inc/chosen.jquery.js"></script>
     <script src="/js/inc/moment.js"></script>
     <script src="/js/inc/datetime.js"></script>
