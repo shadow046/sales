caeff64fb5e845c8caab4b80848bfc20b42dc81d
lancenacabuan <lancenacabuan@outlook.com>
2023-05-29 16:35:10 +0800

    Update

--- resources/views/inc/guest.blade.php
+++ resources/views/inc/guest.blade.php
@@ -1,8 +1,8 @@
 <div class="pb-4">
     <a id="htmlGuest" href="/logout">
         @if(env('APP_SYS') == 'DD')
-            <div style="text-align: center;" class="animate__animated animate__bounceIn">
-                <img class="text-center p-2" src="{{asset('inc/Dunkin.png')}}" style="width: auto; height: 150px;">
+            <div style="text-align: center;">
+                <img class="text-center p-2 animate__animated animate__bounceIn" src="{{asset('inc/Dunkin.png')}}" style="width: auto; height: 150px;">
             </div>
             <div style="height: 15px; background-color: #e11383"></div>
             <div class="xD" style="text-align: center; height: 90px; line-height: 90px; color: white; font-family: Arial; background-color: #683817; font-size: 40px; font-weight: bold;">
@@ -10,8 +10,8 @@
             </div>
             <div style="height: 15px; background-color: #f5821f"></div>
         @else
-            <div style="text-align: center; background-color: #850708"  class="animate__animated animate__bounceIn">
-                <img class="text-center p-2" src="{{env('APP_URL')}}/inc/MaryGrace.jpg" style="width: auto; height: 150px;">
+            <div style="text-align: center; background-color: #850708">
+                <img class="text-center p-2 animate__animated animate__bounceIn" src="{{env('APP_URL')}}/inc/MaryGrace.jpg" style="width: auto; height: 150px;">
             </div>
             <div class="xD" style="text-align: center; height: 90px; line-height: 90px; font-family: Arial; color: #850708; background-color: #fbb12d; font-size: 40px; font-weight: bold;">
                 HEADQUARTERS CONSOLE SYSTEM
